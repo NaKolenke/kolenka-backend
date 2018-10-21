96ca1473ba2118efbd790c6a3cb14434fe7fc960
Pavel <useless.dinosaur@veloc1.me>
2018-10-22 00:40:39 +0300

    Different config for different envs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+app.config.from_object('src.config.default.Config')
+if ('KOLENKA_BACKEND_CONFIG' in os.environ):
+    app.config.from_object('src.config.prod.Prod')
 
 @app.route("/")
 def hello():
