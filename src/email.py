cadafca01c30a529e66cff00bb2645d89a0eec57
Pavel <bartashuk.p@r-mades.com>
2019-07-22 18:56:52 +0400

    Password recovery, email fix

--- src/email.py
+++ src/email.py
@@ -12,12 +12,14 @@ class EmailSender():
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.login(self.sender, self.password)
 
+        msg = ('From: ' + self.sender + '\r\n' +
+               'To: ' + to + '\r\n' +
+               'Subject: ' + subject + '\r\n\r\n' +
+               text)
+
         server.sendmail(
             self.sender,
             to,
-            'From: ' + self.sender + '\r\n' +
-            'To: ' + to + '\r\n' +
-            'Subject: ' + subject + '\r\n\r\n' +
-            text)
+            msg.encode("utf8"))
 
         server.quit()
